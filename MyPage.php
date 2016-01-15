f05d000215e266c1b397939d8799b55451575206
Jonathan Langford <jlangf16@bruinmail.slcc.edu>
2016-01-14 19:15:33 -0700

    Started php augment

--- MyPage.php
+++ MyPage.php
@@ -1,12 +1,72 @@
 <!DOCTYPE html>
-<html>
-    <head>
-            <meta charset="UTF-8">
-            <title>About Me</title>
-    </head>
-    <body>
-        <?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Jonathan Langford</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        echo "Hello World";
+        $greeting = "PHP is Fun!";
+        echo $greeting;
+    ?>
+<div id="wrap">
+    <header>
+        <a href="#"><img src="https://s3-us-west-2.amazonaws.com/site-images-pics/progress.PNG"/></a>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Mission</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">About me</a></li>
+        </ul>
+    </nav>
+    <section class="openStatement">
+        <p style="text-indent: 50px">Welcome to my profile website. I've created this site from scratch as a personal project and goal of mine. Please take a look around and feel free to post any comments or questions you may have.</p>
+        <p style="text-indent: 50px">Progress = Happiness is a wonderful belief I'm referencing from Anthony Robbins. I attended one of his seminars years ago and have lived by this belief ever since.</p>
+    </section>
+    <aside class="sidebar">
+        <div class="profile">
+            <img src=""/>
+        </div>
+    </aside>
 
-        ?>
-    </body>
+    <div class="section">
+        <h3>Mission</h3>
+        <img src="https://s3-us-west-2.amazonaws.com/site-images-pics/build.png" width="320" height="300"/>
+        <p>Build Yourself daily. That's right. Work on yourself daily, mentally and physically. If you are going to watch TV then watch a documentary or something educational.
+            Not the news. Taking small productive steps daily will have a massive impact on your well being.</p>
+    </div>
+
+    <div class="section">
+        <h3>Projects</h3>
+        <img src="https://s3-us-west-2.amazonaws.com/site-images-pics/work.png"/>
+        <p>Projects in Progress. Setup projects to work on. I find rather than watching instructional videos on how to learn something, have a endgame in mind.
+            For example, rather then learning HTML through videos, plan to build a website. Having this goal gives you drive and something to strive for.</p>
+    </div>
+
+    <div class="section">
+        <h3>About Me</h3>
+        <img src="https://s3-us-west-2.amazonaws.com/site-images-pics/personal" width="220" height="300"/>
+        <p>I am a motivated learner, father of four amazing children, and loving husband.</p>
+    </div>
+
+    <footer>
+        <div class="footer" >
+            <p>Please submit any suggestions for building a better website or questions you may have.
+                Thank you for stopping by.</p>
+        </div>
+
+        <div class="socialLinks">
+            <ul>
+                <li><a href="https://github.com/Jonathan166"><img src="https://github-jobs.s3.amazonaws.com/168d1ca6-dd19-11e2-8a2e-153f588b3d80.png" height="100" width="100"/></a></li>
+                <li><a href="https://www.linkedin.com/in/jonathan-langford-304281b8"><img src="https://pbs.twimg.com/profile_images/614583061448036352/CBpFkPaz.png" height="100" width="100"/></a></li>
+            </ul>
+        </div>
+    </footer>
+</div>
+</body>
 </html>
